26d8d1ed82fa3091a520507f5132c267183d6e4f
Kasper - PC <kasper.sapala@gmail.com>
2018-11-22 00:22:23 +0100

    init Factory

--- src/main/cs/agh/judges/Judge.java
+++ src/main/cs/agh/judges/Judge.java
@@ -1,6 +1,8 @@
 package cs.agh.judges;
 
 
+import java.util.Objects;
+
 public class Judge {
 
     public String name;
@@ -8,11 +10,22 @@ public class Judge {
     public JudgesSpecialRoles[] specialRoles;
 
 
-    public void Judge(String name, String function, JudgesSpecialRoles[] specialRoles) {
+    public Judge(String name, String function, JudgesSpecialRoles[] specialRoles) {
         this.name = name;
         this.function = function;
         this.specialRoles = specialRoles;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Judge judge = (Judge) o;
+        return Objects.equals(name, judge.name);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
--- src/main/cs/agh/judges/JudgementFactory.java
+++ src/main/cs/agh/judges/JudgementFactory.java
@@ -1,4 +1,13 @@
 package cs.agh.judges;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class JudgementFactory {
+
+    public Set<Judge> judges = new HashSet<>();
+    public Set<Judgement> judgements = new HashSet<>();
+
 }
